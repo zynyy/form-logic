a53bd4c0d40fc08f3895d31fbbabb88e1c0ca079
hyl <pkuutia70122991@163.com>
2023-02-14 00:27:13 +0800

    chore(release): 😊 publish 1.0.2-alpha.0

--- packages/component/src/style.ts
+++ packages/component/src/style.ts
@@ -1,5 +1,5 @@
 // auto generated code
-
+export default {};
 import './components/ali-react-table/style/index.css';
 import './components/split-pane/style/index.css';
 import './layout/style/index.css';
